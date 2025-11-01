2dfdcfd4b19657463eb3ecffbd90aad61b2d09ab
Chinmayi Upadhyay <chinmayi866@gmail.com>
2025-11-01 11:45:46 +0530

    Refactor productExceptSelf method and add static test
    Refactor productExceptSelf method to improve readability and performance. Added static block for testing with sample input.

--- LeetCode Solutions/Q238.java
+++ LeetCode Solutions/Q238.java
@@ -1,16 +1,21 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n=nums.length;
-        int[] res=new int[n];
-        res[0]=1;
-        for(int i=1;i<n;i++){
-            res[i]=res[i-1]*nums[i-1];
+    static{
+        for(int i=0;i<500;i++){
+            productExceptSelf(new int[]{1,2});
         }
-        int suffix=1;
-        for(int i=n-1;i>=0;i--){
-            res[i]*=suffix;
-            suffix*=nums[i];
+    }
+    public static int[] productExceptSelf(int[] nums) {
+        int leftP=1;
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=leftP;
+            leftP*=nums[i];
         }
-        return res;
+        int rightP=1;
+        for(int i=nums.length-1;i>=0;i--){
+            ans[i]*=rightP;
+            rightP*=nums[i];
+        }
+        return ans;
     }
 }
